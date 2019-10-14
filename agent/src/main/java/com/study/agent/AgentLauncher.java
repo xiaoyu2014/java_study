c4a0101ebfb1dcad9d251d214df3486bd64301ed
yuqi12 <yuqi12@meituan.com>
2019-10-14 18:51:53 +0800

    save

---
+++ agent/src/main/java/com/study/agent/AgentLauncher.java
@@ -0,0 +1,18 @@
+package com.study.agent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @Author: yuqi
+ * @Date: 2019-10-14 16:54
+ */
+public class AgentLauncher {
+
+    public static void premain(String featureString, Instrumentation inst) {
+    }
+
+    public static void agentmain(String featureString, Instrumentation inst) {
+
+    }
+
+}
---
+++ core/src/main/java/com/study/core/CoreLauncher.java
@@ -0,0 +1,58 @@
+package com.study.core;
+
+
+import com.sun.tools.attach.VirtualMachine;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @Author: yuqi
+ * @Date: 2019-10-14 16:55
+ */
+public class CoreLauncher {
+
+    public CoreLauncher(final String targeJvmPid,
+                        final String agentJarPath,
+                        final String token) throws Exception{
+        //加载agent
+
+    }
+
+    //加载agent
+    public void attachAgent(final String targetJvmPid,
+                            final String agentJarPath,
+                            final String cfg) throws Exception{
+
+        VirtualMachine virtualMachine = null;
+        try{
+            virtualMachine = VirtualMachine.attach(targetJvmPid);
+            if(virtualMachine != null){
+                virtualMachine.loadAgent(agentJarPath, cfg);
+            }
+        }finally {
+            if(null != virtualMachine){
+                virtualMachine.detach();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try {
+
+            // check args
+            if (args.length != 3
+                    || StringUtils.isBlank(args[0])
+                    || StringUtils.isBlank(args[1])
+                    || StringUtils.isBlank(args[2])) {
+                throw new IllegalArgumentException("illegal args");
+            }
+
+            new CoreLauncher(args[0], args[1], args[2]);
+        } catch (Throwable t) {
+            t.printStackTrace(System.err);
+            System.err.println("core load jvm failed : " + t.getMessage());
+            System.exit(-1);
+        }
+    }
+
+}
